bc95d7b028d8662c8ee58b69274e705a6bf898dd
Gvln <santis.paez2414@outlook.com>
2024-10-07 15:14:43 -0500

    develop: new start for renderEngine

--- app/src/main/java/org/example/mainClass/Main.java
+++ app/src/main/java/org/example/mainClass/Main.java
@@ -1,11 +1,7 @@
 package org.example.mainClass;
 
-import org.example.map.Map;
-import org.example.map.MapSectors;
-import org.example.player.Player;
 import org.example.renderEngine.DisplayMananger;
 import org.example.renderEngine.Inputs;
-import org.example.renderEngine.RaysDraw;
 import org.lwjgl.glfw.GLFW;
 
 import org.lwjgl.opengl.GL11;
@@ -13,14 +9,8 @@ import org.lwjgl.opengl.GL11;
 // basic stuff: start (for thread), init (create window) <- run <- (init, update, render)
 
 public class Main implements Runnable {
-  private final int WIDTHRAYCAST2D = 1700, HEIGHTRAYCAST2D = 1000, WIDTHGAME3D = 1024, HEIGHTGAME3D = 576;
-  private DisplayMananger displayRayCast2D;
-  private DisplayMananger displayGame3D;
-  private Player player;
-  private Map map;
-  private RaysDraw raysDraw; 
-
-  private MapSectors mapSectors;
+  private final int WIDTHGAME = 1024, HEIGHTGAME = 576;
+  private DisplayMananger displayGame;
 
   // Thread help us to run the same code at the same time
   public Thread threadOne;
@@ -31,22 +21,16 @@ public class Main implements Runnable {
     // the thread use run cause main implements runnable
     threadOne = new Thread(this, "first thread");
     threadOne.start();
-
-    this.player = new Player(300, 300);
-    this.map = new Map();
-    this.raysDraw = new RaysDraw(player, map, WIDTHGAME3D);
-
-    this.mapSectors = new MapSectors();
   }
 
   public void init() {
     System.out.println("init new game");
     // crate the window with DisplayMananger class
-    displayRayCast2D = new DisplayMananger(WIDTHRAYCAST2D, HEIGHTRAYCAST2D, "RayCasting2D");
-    displayRayCast2D.createDisplay();
+    // displayRayCast2D = new DisplayMananger(WIDTHRAYCAST2D, HEIGHTRAYCAST2D, "RayCasting2D");
+    // displayRayCast2D.createDisplay();
 
-    displayGame3D = new DisplayMananger(WIDTHGAME3D, HEIGHTGAME3D, "Game3D");
-    displayGame3D.createDisplay();
+    displayGame = new DisplayMananger(WIDTHGAME, HEIGHTGAME, "Game");
+    displayGame.createDisplay();
 
   }
 
@@ -54,49 +38,35 @@ public class Main implements Runnable {
     init();
     System.out.println("runnig new game");
     // while is not close -> update, render
-    while (!displayRayCast2D.closeDisplay() && !displayGame3D.closeDisplay()) {
+    while (!displayGame.closeDisplay()) {
       update();
       render();
       // exit while if ESCAPE is press
       if (Inputs.isKeyDown(GLFW.GLFW_KEY_ESCAPE)) return;
     }
-    displayRayCast2D.destroyFree();
-    displayGame3D.destroyFree();
+    displayGame.destroyFree();
   }
 
   private void update() {
-    displayRayCast2D.updateDisplay();
-    displayGame3D.updateDisplay();
+    displayGame.updateDisplay();
   }
 
   private void render() {
     // render 2D
-    GLFW.glfwMakeContextCurrent(displayRayCast2D.getDisplay());
+    GLFW.glfwMakeContextCurrent(displayGame.getDisplay());
     GL11.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
-    mapSectors.drawMapSectors();
-
-    displayRayCast2D.swapBuffers(); 
+    displayGame.swapBuffers(); 
 
     // render 3D
-    GLFW.glfwMakeContextCurrent(displayGame3D.getDisplay());
-    GL11.glClearColor(0.48f, 0.50f, 0.52f, 1.0f);
-    GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+    //GLFW.glfwMakeContextCurrent(displayGame3D.getDisplay());
+    // GL11.glClearColor(0.48f, 0.50f, 0.52f, 1.0f);
+    // GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
-    raysDraw.drawRays("raysuse3d");
-
-
-    displayGame3D.swapBuffers(); 
-  }
-
-  public int getWIDTHGAME3D() {
-    return WIDTHGAME3D;
   }
 
   public static void main(String[] args) {
     new Main().start();
   }
-
-
 }
